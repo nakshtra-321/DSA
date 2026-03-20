07ff3842f86a2c8715b072fb1b2adf4dd0558498
nakshtra-321 <nakshtra.26327@stu.upes.ac.in>
2026-03-20 08:49:02 +0530

    day_45.c

---
+++ day_45.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1) nodes[i] = NULL;
+        else nodes[i] = newNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+            if (l < n) nodes[i]->left = nodes[l];
+            if (r < n) nodes[i]->right = nodes[r];
+        }
+    }
+
+    return nodes[0];
+}
+
+int height(struct Node* root) {
+    if (root == NULL) return 0;
+    int lh = height(root->left);
+    int rh = height(root->right);
+    return (lh > rh ? lh : rh) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    printf("%d", height(root));
+
+    return 0;
+}
---
+++ main code/day_45.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left, *right;
+};
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct TreeNode* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1) nodes[i] = NULL;
+        else nodes[i] = newNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+            if (l < n) nodes[i]->left = nodes[l];
+            if (r < n) nodes[i]->right = nodes[r];
+        }
+    }
+
+    return nodes[0];
+}
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) return 0;
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct TreeNode* root = buildTree(arr, n);
+
+    printf("%d", maxDepth(root));
+
+    return 0;
+}
