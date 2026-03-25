cfd2c0d5c0914d101e7786e76e86c297965369ae
nakshtra-321 <nakshtra.26327@stu.upes.ac.in>
2026-03-25 22:08:14 +0530

    Create day_46.c

---
+++ main code/day_46.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int val;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1) nodes[i] = NULL;
+        else nodes[i] = newNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+            if (l < n) nodes[i]->left = nodes[l];
+            if (r < n) nodes[i]->right = nodes[r];
+        }
+    }
+
+    return nodes[0];
+}
+
+void levelOrder(struct Node* root) {
+    if (root == NULL) return;
+
+    struct Node* queue[2000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int size = rear - front;
+        for (int i = 0; i < size; i++) {
+            struct Node* curr = queue[front++];
+            printf("%d ", curr->val);
+
+            if (curr->left) queue[rear++] = curr->left;
+            if (curr->right) queue[rear++] = curr->right;
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    levelOrder(root);
+
+    return 0;
+}
